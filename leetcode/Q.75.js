d1ff309209d0190e7946778296a0d190c7b2e708
ni2-vsv11 <ni2educational@gmail.com>
2025-06-27 16:38:52 +0530

    Leetcode Q.75 Solved

---
+++ leetcode/Q.75.js
@@ -0,0 +1,25 @@
+// Sort Colors - Med
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let i=0, j=0, k=nums.length-1;
+    
+    while(i<=k){
+        if(nums[i]==0){
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++;
+            j++;
+        }
+        else if(nums[i]==2){
+            [nums[i], nums[k] ] = [nums[k], nums[i]]
+            k--;
+        }
+        else{
+            i++;
+        }
+    } 
+    console.log(nums);
+};
